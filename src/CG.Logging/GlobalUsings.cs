64513bd764bcc2165c1399cc5249fbbbbac688ea
Martin Cook <33993749+martycook@users.noreply.github.com>
2022-11-15 14:30:16 -0600

    I removed the configuration extensions

--- src/CG.Logging/GlobalUsings.cs
+++ src/CG.Logging/GlobalUsings.cs
@@ -1,6 +1,2 @@
-﻿global using CG.Validations;
-global using Microsoft.AspNetCore.Builder;
-global using Microsoft.AspNetCore.Hosting;
-global using Microsoft.Extensions.Configuration;
-global using System;
+﻿global using System;
 global using System.Diagnostics;
